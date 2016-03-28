56d5913fb3acd17812006ddff4b27ead9e92e28f
Alex Thomas <Alex Thomas>
2016-03-28 22:14:47 +1300

    view page now lets you view entire documents

--- routes/view.js
+++ routes/view.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var basex = require('basex');
+var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
+client.execute("OPEN Colenso");
 
 router.get('/', function(req, res, next) {
-    console.log(req.originalUrl);
-    res.render('view', { title: 'Colenso Project Search'});
+    var rootPath = "./Colenso/"
+    var docPath = req.query.path
+    console.log(docPath)
+    client.execute(
+        "XQUERY doc('" + rootPath + docPath + "')",
+        function (error, result){
+            if(error){ console.error(error);}
+            else{
+                res.render('view', {
+                    title: 'Colenso Project view',
+                    document:result.result,
+                    docPath:docPath
+                });
+            }
+        }
+    )
+
 });
 
 module.exports = router;
