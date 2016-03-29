a736cbef51a8677447aacd67adf27d29f143f027
Alex Thomas <Alex Thomas>
2016-03-29 13:35:06 +1300

    can now use XPath queries to search for attributes in tags

--- routes/results.js
+++ routes/results.js
@@ -8,13 +8,29 @@ client.execute("OPEN Colenso");
 
 router.get('/', function(req, res, next) {
     var searchString = req.query.text
+    var xPath = req.query.xPath
+    if(xPath == undefined || xPath == ""){
+        xPath = "";
+    }else if(xPath[0] == "["){
+        xPath = xPath
+    }else{
+        xPath = "[" + xPath + "]";
+    }
     var rootPath = "./Colenso/"
     client.execute(
         "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
-        " for $n in (//" + searchString +")[position() = (1 to 10)]\n"+
+        " for $n in (//" + searchString + xPath + ")[position() = (1 to 10)]\n"+
         "return $n\n",
         function (error, result) {
-            if(error){ console.error(error);}
+            if(error){
+                console.error(error);
+                res.render('results',{
+                    title: 'Colenso Project Search Results',
+                    xPath: xPath,
+                    searchString: searchString,
+                    failed: 'true'
+                })
+            }
             else {
                 var stuff = result.result
                 stuff = stuff.substring(1 + searchString.length,stuff.length)
@@ -24,10 +40,18 @@ router.get('/', function(req, res, next) {
                 }
                 client.execute(
                     "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';"+
-                    "for $n in (//" + searchString +")[position() = 1 to 10]\n" +
+                    "for $n in (//" + searchString + xPath + ")[position() = 1 to 10]\n" +
                     "return db:path($n)",
                     function (error, result){
-                        if(error){ console.error(error);}
+                        if(error){
+                            console.error(error);
+                            res.render('results',{
+                                title: 'Colenso Project Search Results',
+                                xPath: xPath,
+                                searchString: searchString,
+                                failed: 'true'
+                            })
+                        }
                         else{
                             var docPaths = result.result
                             docPaths = docPaths.split("\n")
@@ -36,7 +60,8 @@ router.get('/', function(req, res, next) {
                                 query: stuff,
                                 searchString: searchString,
                                 rootPath: rootPath,
-                                docPaths: docPaths
+                                docPaths: docPaths,
+                                xPath: xPath
                             });
                         }
                     }
