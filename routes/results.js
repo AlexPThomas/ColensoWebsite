4fbfefd6fb39996ea1b56ff0355e59571935409a
Alex Thomas <Alex Thomas>
2016-03-29 15:51:43 +1300

    added the ability to go to the next page of results

--- routes/results.js
+++ routes/results.js
@@ -7,19 +7,31 @@ client.execute("OPEN Colenso");
 
 
 router.get('/', function(req, res, next) {
-    var searchString = req.query.text
-    var xPath = req.query.xPath
+    var searchString = req.query.text;
+    var count = req.query.count;
+    var countString = "";
+    if(count == undefined || count == ""){
+        count = 1;
+    }
+    count = parseInt(count)
+    countString = count.toString() + " to " + (count + 9).toString();
+    console.log(count);
+    console.log(countString);
+    var xPath = req.query.xPath;
     if(xPath == undefined || xPath == ""){
         xPath = "";
     }else if(xPath[0] == "["){
-        xPath = xPath
+        xPath = xPath;
     }else{
         xPath = "[" + xPath + "]";
     }
-    var rootPath = "./Colenso/"
+
+    var rootPath = "./Colenso/";
+
+
     client.execute(
         "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
-        " for $n in (//" + searchString + xPath + ")[position() = (1 to 10)]\n"+
+        " for $n in (//" + searchString + xPath + ")[position() =(" + countString+")]\n"+
         "return $n\n",
         function (error, result) {
             if(error){
@@ -32,6 +44,7 @@ router.get('/', function(req, res, next) {
                 })
             }
             else {
+                console.log(result.result)
                 var stuff = result.result
                 stuff = stuff.substring(1 + searchString.length,stuff.length)
                 stuff = stuff.split("<" + searchString)
@@ -40,7 +53,7 @@ router.get('/', function(req, res, next) {
                 }
                 client.execute(
                     "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';"+
-                    "for $n in (//" + searchString + xPath + ")[position() = 1 to 10]\n" +
+                    "for $n in (//" + searchString + xPath + ")[position() = (" + countString+")]\n" +
                     "return db:path($n)",
                     function (error, result){
                         if(error){
@@ -55,13 +68,16 @@ router.get('/', function(req, res, next) {
                         else{
                             var docPaths = result.result
                             docPaths = docPaths.split("\n")
+                            console.log(xPath)
+                            console.log(searchString)
                             res.render('results', {
                                 title: 'Colenso Project Search Results',
                                 query: stuff,
                                 searchString: searchString,
                                 rootPath: rootPath,
                                 docPaths: docPaths,
-                                xPath: xPath
+                                xPath: xPath,
+                                count: count
                             });
                         }
                     }
